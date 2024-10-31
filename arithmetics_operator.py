952163aad117fac60869317f43b335446aa2fb88
lorenzo <lorenzo.rsp@gmail.com>
2024-10-31 02:13:30 +0100

    inserimento file aritmetica ed operatori

---
+++ arithmetics_operator.py
@@ -0,0 +1,38 @@
+import math
+
+# Some Math functions and math operators
+# + - * / 
+print(1 + 1)
+print(1 - 1)
+print(2 * 2)
+print(6 / 2)
+
+# %
+print("2 elevato alla 3: ", 2^3)
+
+# += *= /= -=
+print(1 + 1)
+print(1 + 1)
+print(1 + 1)
+print(1 + 1)
+
+# round() abs() pow() 
+print(round(3,14))
+print(abs(-4))
+print(pow(2, 3))
+
+# max() min()
+x = 2
+y = 3
+z = 5
+print(max(x,y,z))
+print(min(x,y,z))
+
+# funzioni importate da modulo math
+print(math.pi)
+print(math.e)
+print(math.sqrt(121))
+print(math.ceil(9.8))
+print(math.floor(9.8))
+print(math.sin(90))
+print(math.cos(90))
--- conditions-if-else.py
+++ conditions-if-else.py
@@ -1,6 +1,7 @@
-# Condizioni con if e else
+# Condizioni con if, elif, else
 
 age = 18
 
 if(age < 18): print("Sei minorenne")
-else: print("sei maggiorenne")
+elif(age >= 18 & age < 21): print("Sei maggiorenne ma non puoi bere")
+else: print("sei maggiorenne e puoi fare come ti pare")
